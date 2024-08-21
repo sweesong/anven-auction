a2cfa244191cf6711eb52985e08108960eadd751
Swee Song <sweesong@gmail.com>
2024-08-21 16:27:57 +0800

    fix minor landing page UI

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,13 @@ import { subtitle, title } from "@/components/primitives";
 import { Button } from "@nextui-org/button";
 import { Image } from "@nextui-org/image";
 import { Link } from "@nextui-org/link";
+import { Spacer } from "@nextui-org/spacer";
 
 export default async function Home() {
 
   return (
+    <>
+    <Spacer className="lg:hidden" y={10} />
     <div className="flex flex-col justify-center gap-5 pt-4">
       <div className="border justify-center">
       <h1 className={title()}>Discover & &nbsp;</h1>
@@ -18,12 +21,13 @@ export default async function Home() {
       <Image
         shadow="lg"
         alt="Card background"
-        className="z-0 h-[350px] w-full object-cover"
+        className="z-0 w-full object-cover"
         src="landing_bg1.png"
       />
       <div className="flex justify-center">
         <Button as={Link} color="warning" variant="shadow" className="w-[200px]" href="/listings">Browse Auction Listing</Button>
       </div>
     </div>
+    </>
   )
 }
