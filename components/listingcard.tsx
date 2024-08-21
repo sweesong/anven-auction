4020fa77550df369e53e16e33b0eb689ecc264f4
Swee Song <sweesong@gmail.com>
2024-08-21 23:00:06 +0800

    clean up source code

--- components/listingcard.tsx
+++ components/listingcard.tsx
@@ -2,13 +2,12 @@ import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
 import { Chip } from "@nextui-org/chip";
 import { Divider } from "@nextui-org/divider";
 import { Image } from "@nextui-org/image";
-import { BedSingleIcon, CalculatorIcon, GavelIcon} from "lucide-react";
+import { CalculatorIcon, GavelIcon} from "lucide-react";
 import { WhatsAppIcon } from "./icons";
 import { Link } from "@nextui-org/link";
 import { AuctionListing } from "../lib/definitions";
 import { formatDateToStr } from "@/lib/utils";
 import { Tooltip } from "@nextui-org/tooltip";
-
 
 interface ListingCardProps {
     listing: AuctionListing;
@@ -83,46 +82,3 @@ export default function ListingCard({ listing }: ListingCardProps) {
 
     )
 }
-
-
-/*
-<div className="border border-gray-300 max-w-lg w-full bg-white rounded-lg shadow-lg overflow-hidden">
-            <div className="relative w-full max-w-sm mx-auto">
-                <Chip variant="shadow" color="warning" className="absolute top-2 left-2 text-white px-2 py-2">{listing.type}</Chip>
-                <Image className="w-full h-48 object-cover" src={listing.img} alt="Property Image" />
-                <RectangleEllipsisIcon className="text-gray-400 absolute bottom-2 right-2 bg-white p-1 rounded-full" />
-            </div>
-            <div className="flex flex-col p-4">
-                <div className="flex-1 flex flex-col justify-between min-h-[160px]">
-                    <div className="flex flex-col p-1 mt-auto min-h-[60px] gap-1">
-                        <div className="text-gray-800">{listing.title}</div>
-                    </div>
-                    <div className="flex-1 flex-col p-1 mt-auto min-h-[20px] gap-1">
-                        <div className="flex flex-row items-center justify-between text-gray-800 font-semibold gap-1">
-                            <div>RM {listing.price.toLocaleString('en-US')}</div>
-                            <div className="flex gap-1 text-sm items-center">
-                                <GavelIcon size={14} />
-                                28 AUG 2024 (MON)
-                            </div>
-                        </div>
-                        <div className="text-gray-600 pt-2 text-xs text-gray-400">{listing.address}</div>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-2">
-                    <Divider/>
-                    <div className="flex items-center justify-between gap-1 text-xs text-gray-700">
-                        <div className="flex gap-1  items-center">
-                            *Freehold
-                        </div>
-                        <div className="flex gap-1 items-center">
-                            *LA/BU: 3,498sf/3,064sf
-                        </div>
-                        <div className="flex gap-1 items-center">
-                            *4+1r/5b
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-*/
-
