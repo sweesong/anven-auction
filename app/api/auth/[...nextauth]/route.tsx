87e2e03e52deaaa6bacfae79ad21aa34ddd96570
Swee Song <sweesong@gmail.com>
2024-09-08 01:17:23 +0800

    remove AuthOptions

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,12 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
-import { Account, User as AuthUser } from "next-auth";
+import { User as AuthUser } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 const prisma = new PrismaClient();
 
-export const authOptions: any = {
-  // Configure one or more authentication providers
+export const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
@@ -14,29 +13,33 @@ export const authOptions: any = {
     }),
   ],
   callbacks: {
-    async signIn({ user, account }: { user: AuthUser; account: Account }) {
+    async signIn({ user }: { user: AuthUser }) {
       try {
-        console.log(user.email);
-        
-        if (user.email == null) {
-          throw new Error('Email cannot be null');
+        // Ensure the email is not null or undefined
+        if (!user.email) {
+          throw new Error('Email cannot be null or undefined');
         }
 
+        // Find the user in the database
         const existingUser = await prisma.user.findUnique({
           where: { email: user.email },
         });
 
+        // Check if user exists
         if (!existingUser) {
           throw new Error("You are not authorized to access the dashboard.");
         }
-        
+
         return true;
       } catch (err) {
+        // Return the error message and redirect to error page
         return `/0191ba6b-7443-75f3-8c5c-da766df93c5e?error=${encodeURIComponent((err as Error).message)}`;
+      } finally {
+        // Ensure Prisma Client is closed
+        await prisma.$disconnect();
       }
     },
   },
-};
+});
 
-export const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
